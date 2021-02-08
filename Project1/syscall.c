fd0b07d49c12c2ea99744d8107ceaff94c9dd3fb
Matthew Notaro <matthew.y.notaro@gmail.com>
2021-02-08 18:03:04 -0500

    finished syscall

--- Project1/syscall.c
+++ Project1/syscall.c
@@ -12,21 +12,32 @@
 #include <stdlib.h>
 #include <sys/time.h>                                                                                
 #include <sys/syscall.h>
+#include <unistd.h>
 
 double avg_time = 0;
 
 int main(int argc, char *argv[]) {
+    // Initialize start and stop structs
+    struct timeval start, stop;
 
-    /* Implement Code Here */
-    float avg_time = 0;
+    // Sets initial time
+    gettimeofday(&start, NULL);
+
+    // System call loop
     int i;
-    for(i = 0; i < 1000; i++){
-
-        avg_time += 0;
+    pid_t pid;
+    for(i = 1; i <= 10000; i++){
+        pid = syscall(SYS_getpid);
     }
-    // Remember to place your final calculated average time
-    // per system call into the avg_time variable
 
+    // Sets final time
+    gettimeofday(&stop, NULL);
+
+    // Calculates microsecond time difference and average
+    double total_time = (double)(stop.tv_usec - start.tv_usec) + (double)(stop.tv_sec - start.tv_sec) * 1000000;
+    avg_time = total_time/i;
+
+    // Prints average time per system call
     printf("Average time per system call is %f microseconds\n", avg_time);
 
     return 0;
