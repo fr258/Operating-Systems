60c3c10a0026475be140fdfe045d519decea2dd1
Matthew Notaro <matthew.y.notaro@gmail.com>
2021-02-12 16:22:12 -0500

    readded .gitignore and added comment to signal

--- Project1/signal.c
+++ Project1/signal.c
@@ -1,9 +1,22 @@
+/* syscall.c
+ *
+ * Group Members Names and NetIDs:
+ *   1. Matthew Notaro (myn7)
+ *   2. Farrah Rahman (fr258)
+ *
+ * ILab Machine Tested on:
+ *   kill.cs.rutgers.edu
+ */
+
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-// Changes the stack frame of caller (main function)
-// such that you get to the line after "r2 = *( (int *) 0 )"
+
+/* Part 1 - Step 2 to 4: Do your tricks here
+ * Your goal must be to change the stack frame of caller (main function)
+ * such that you get to the line after "r2 = *( (int *) 0 )"
+ */
 void segment_fault_handler(int signum) {
 
     printf("I am slain!\n");
@@ -21,7 +34,7 @@ int main(int argc, char *argv[]) {
 
     int r2 = 0;
 
-    // Register segfault signal handler
+    /* Part 1 - Step 1: Registering signal handler */
     signal(SIGSEGV, segment_fault_handler);
 
     // This will generate segmentation fault
