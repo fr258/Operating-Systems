ba547a03bce67562aa85ea107cf5b12d40df4b6f
Matthew Notaro <matthew.y.notaro@gmail.com>
2021-02-12 15:21:02 -0500

    finished part 1

--- Project1/signal.c
+++ Project1/signal.c
@@ -2,30 +2,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* Part 1 - Step 2 to 4: Do your tricks here
- * Your goal must be to change the stack frame of caller (main function)
- * such that you get to the line after "r2 = *( (int *) 0 )"
- */
+// Changes the stack frame of caller (main function)
+// such that you get to the line after "r2 = *( (int *) 0 )"
 void segment_fault_handler(int signum) {
 
     printf("I am slain!\n");
 
+    int *p;
 
-    /* Implement Code Here */
+    // Move pointer up to segfaulting address in main.
+    p = &signum + 0x33;
 
-
+    // Increment past segfaulting instruction up to print statement.
+    *p += 0x5;
 }
 
 int main(int argc, char *argv[]) {
 
     int r2 = 0;
 
-    /* Part 1 - Step 1: Registering signal handler */
-    /* Implement Code Here */
+    // Register segfault signal handler
+    signal(SIGSEGV, segment_fault_handler);
 
-    r2 = *( (int *) 0 ); // This will generate segmentation fault
+    // This will generate segmentation fault
+    r2 = *( (int *) 0 );
 
+    // Target statement
     printf("I live again!\n");
 
-    return ;
+    return 0;
 }
